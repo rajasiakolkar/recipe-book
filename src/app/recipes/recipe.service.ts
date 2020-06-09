d5708aed4d1a55f5dad498c12725ef0de593b05c
Rajasi <akolkar.r@husky.neu.edu>
2020-06-09 14:30:41 -0400

    adding Add to Cart functionality

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,40 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter, Output } from '@angular/core';
+import { EventEmitter, Output, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
+
+   constructor(private slService: ShoppingListService) {}
 
   @Output()
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('Test', 'Test Desc', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg'),
-    new Recipe('Test2', 'Test Desc2', 'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg')
+    new Recipe('Test',
+    'Test Desc',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg',
+    [
+      new Ingredient('abc', 1),
+      new Ingredient('xyz', 10)
+    ]),
+    new Recipe('Test2',
+    'Test Desc2',
+    'https://www.bbcgoodfood.com/sites/default/files/recipe-collections/collection-image/2013/05/chorizo-mozarella-gnocchi-bake-cropped.jpg',
+    [
+      new Ingredient('abc', 2),
+      new Ingredient('xyz', 30)
+    ])
   ];
 
   getRecipes() {
     //we only get a copy and not direct reference to the array
     return this.recipes.slice();
+  }
+
+  addToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 
 }

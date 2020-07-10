c8a24154cdc98b75e0147c8fdc7c0c29a638abdf
Rajasi <akolkar.r@husky.neu.edu>
2020-07-10 15:11:21 -0400

    Optimizing Angular with Lazy Loading

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { DropdownDirective } from './dropdown.directive';
+
+@NgModule({
+  declarations:[
+    LoadingSpinnerComponent,
+    DropdownDirective
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    LoadingSpinnerComponent,
+    CommonModule,
+    DropdownDirective
+  ]
+})
+export class SharedModule {
+
+}
